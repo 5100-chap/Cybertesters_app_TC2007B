a49584acbe51c0175422a67b0324d1a5a5fe0cb0
AlessandroTH22 <111011715+AlessandroTH22@users.noreply.github.com>
2022-10-21 13:50:51 -0500

    Add files via upload

---
+++ appProyecto/appProyecto/ViewControllerContra.swift
@@ -0,0 +1,28 @@
+//
+//  ViewControllerContra.swift
+//  appProyecto
+//
+//  Created by Alessandro Tolentino Hernandez on 17/10/22.
+//
+
+import UIKit
+
+class ViewControllerContra: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    // MARK: - Navigation
+
+     @IBAction func BotonRegresar(_ sender: UIButton) {
+         dismiss(animated: true)
+         
+     }
+     
+
+}
