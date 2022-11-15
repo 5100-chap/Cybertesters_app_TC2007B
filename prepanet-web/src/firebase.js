8aecff5278fbe56d63f7068a505b08fc0991a05b
Diego Alberto Baños Lopez <dbanoslopez@sps-program.com>
2022-11-15 15:04:43 -0600

    avance

---
+++ prepanet-web/src/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBaFiRzQnulTdfREqO0WI-2AcdBLw3PpqA",
+    authDomain: "prepanet-back-cybertesters.firebaseapp.com",
+    projectId: "prepanet-back-cybertesters",
+    storageBucket: "prepanet-back-cybertesters.appspot.com",
+    messagingSenderId: "983693589572",
+    appId: "1:983693589572:web:07f65a52a551fe063a5011"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
