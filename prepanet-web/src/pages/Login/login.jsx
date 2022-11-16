bdfe4a814b866be1a1204c0b9ccf433103dcc9f4
5100-Chap <A01275100@tec.mx>
2022-11-16 16:56:23 -0600

    Avance 2

--- prepanet-web/src/pages/Login/login.jsx
+++ prepanet-web/src/pages/Login/login.jsx
@@ -1,47 +1,63 @@
 import React from 'react';
+import { Helmet } from 'react-helmet'
+import config from '../../settings/config.json'
+
+const Title = config.Login;
 
 class Login extends React.Component {
     render() {
         return (
-            <div>
-                <link href="./css/StyleSheet.css" rel="stylesheet" />
-                <img
-                    src="./images/rectangleLogin.png"
-                    alt="Rectangle33561181"
-                    className="iniciodesesn-administradoresy-coordinadores-rectangle3356"
-                />
-                <div className="iniciodesesn-administradoresy-coordinadores-container">
-                    <span className="iniciodesesn-administradoresy-coordinadores-text">
-                        <span>Log In</span>
-                    </span>
-                    <span className="iniciodesesn-administradoresy-coordinadores-text2">
-                        <span>E-mail</span>
-                    </span>
-                    <span className="iniciodesesn-administradoresy-coordinadores-text4">
-                        <span>Contraseña</span>
-                    </span>
-                    <div className="iniciodesesn-administradoresy-coordinadores-frame11">
-                        <span className="iniciodesesn-administradoresy-coordinadores-text6">
-                            <span>LOG IN</span>
-                        </span>
-                    </div>
-                    <input
-                        type="text"
-                        placeholder="E-mail"
-                        className="iniciodesesn-administradoresy-coordinadores-textinput input"
-                    />
-                    <input
-                        type="text"
-                        placeholder="Contraseña"
-                        className="iniciodesesn-administradoresy-coordinadores-textinput1 input"
-                    />
+            <main>
+                <Helmet>
+                    <title>
+                        {Title}
+                    </title>
+                </Helmet>
+                <div>
+                    <link href="./css/StyleSheet.css" rel="stylesheet" />
                     <img
-                        src="./images/prepanetLogo.png"
-                        alt="prepanetremovebgpreview21181"
-                        className="iniciodesesn-administradoresy-coordinadores-prepanetremovebgpreview2"
+                        src="./images/rectangleLogin.png"
+                        alt="Rectangle33561181"
+                        className="iniciodesesn-administradoresy-coordinadores-rectangle3356"
                     />
+                    <div className="iniciodesesn-administradoresy-coordinadores-container">
+                        <span className="iniciodesesn-administradoresy-coordinadores-text">
+                            <span>Log In</span>
+                        </span>
+                        <span className="iniciodesesn-administradoresy-coordinadores-text2">
+                            <span>E-mail</span>
+                        </span>
+                        <span className="iniciodesesn-administradoresy-coordinadores-text4">
+                            <span>Contraseña</span>
+                        </span>
+                        <div className="iniciodesesn-administradoresy-coordinadores-frame11">
+                            <button
+                                type="button"
+                                id="login-btn"
+                                className="btn">
+                                Log-in
+                            </button>
+                        </div>
+                        <input
+                            type="text"
+                            placeholder="E-mail"
+                            id="usuario-login"
+                            className="iniciodesesn-administradoresy-coordinadores-textinput input"
+                        />
+                        <input
+                            type="text"
+                            placeholder="Contraseña"
+                            id="password-login"
+                            className="iniciodesesn-administradoresy-coordinadores-textinput1 input"
+                        />
+                        <img
+                            src="./images/prepanetLogo.png"
+                            alt="prepanetremovebgpreview21181"
+                            className="iniciodesesn-administradoresy-coordinadores-prepanetremovebgpreview2"
+                        />
+                    </div>
                 </div>
-            </div>
+            </main>
         );
 
     }
